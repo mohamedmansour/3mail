fb4213011c0cf7a8d65733fd51af2bb7e2cdee0d
Mohamed Mansour <hello@mohamedmansour.com>
2021-05-29 16:57:38 -0700

    Change ODBTest to absolute to not intefere with layout

--- frontend/src/components/organisms/OrbitDbTest.tsx
+++ frontend/src/components/organisms/OrbitDbTest.tsx
@@ -1,38 +1,57 @@
 import { Button } from '@chakra-ui/button';
+import { Box } from '@chakra-ui/layout';
 import { useIpfs } from 'contexts/IPFS';
-import { useOrbitDb } from 'contexts/OrbitDB'
+import { useOrbitDb } from 'contexts/OrbitDB';
 import { multiaddr } from 'ipfs-core/src';
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 
 const OrbitDbTest = () => {
-  const {ipfs} = useIpfs();
+  const { ipfs } = useIpfs();
   const { odb, db } = useOrbitDb();
 
   useEffect(() => {
     if (!db) return;
-    db.events.on('replicate', (address: string) => console.debug("replicate", address))
-    db.events.on('replicated', (address: string) => console.log("replicated", address))
-    db.events.on('replicate.progress', (address: string) => console.debug("replicate.progress", address))
-    db.events.on('load', (dbname: string) => console.debug("start loading", dbname))
-    db.events.on('ready', (dbname: string) => console.log("ready", dbname))
-    const all = db.iterator({limit: 10}).collect().map((e: any)  => e.payload);
-    console.log(all)
-
-  },[db])
+    db.events.on('replicate', (address: string) =>
+      console.debug('replicate', address)
+    );
+    db.events.on('replicated', (address: string) =>
+      console.log('replicated', address)
+    );
+    db.events.on('replicate.progress', (address: string) =>
+      console.debug('replicate.progress', address)
+    );
+    db.events.on('load', (dbname: string) =>
+      console.debug('start loading', dbname)
+    );
+    db.events.on('ready', (dbname: string) => console.log('ready', dbname));
+    const all = db
+      .iterator({ limit: 10 })
+      .collect()
+      .map((e: any) => e.payload);
+    console.log(all);
+  }, [db]);
 
   const addSth = async () => {
-    await ipfs!.swarm.connect(multiaddr("/dns4/ipfs.depa.digital/tcp/4002/wss/p2p/QmXAghnP7DqmAEE7Zx4SxMo3UcUVSn8f1xDCT6x1ysYMSj"));
+    await ipfs!.swarm.connect(
+      multiaddr(
+        '/dns4/ipfs.depa.digital/tcp/4002/wss/p2p/QmXAghnP7DqmAEE7Zx4SxMo3UcUVSn8f1xDCT6x1ysYMSj'
+      )
+    );
 
     db.add({
-      "some":"key",
-      "with":"value",
-    })
-  }
+      some: 'key',
+      with: 'value',
+    });
+  };
 
-  return(<div>
-    ODB Test.foo
-    <Button colorScheme="teal"  onClick={() => addSth()}>add sth</Button>
-  </div>)
-}
+  return (
+    <Box position="absolute" bottom={0}>
+      ODB Test.foo
+      <Button colorScheme="teal" onClick={() => addSth()}>
+        add sth
+      </Button>
+    </Box>
+  );
+};
 
 export default OrbitDbTest;
