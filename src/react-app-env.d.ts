1dca04016307cb5e3b0bfbb34fccf8dab6336d0b
Mohamed Mansour <hello@mohamedmansour.com>
2021-05-28 17:04:31 -0700

    Install Ceramic IPFS

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="react-scripts" />
+
+declare module 'uint8arrays' {
+  export function toString(b: Uint8Array, enc?: string): string
+  export function fromString(s: string, enc?: string): Uint8Array
+}
